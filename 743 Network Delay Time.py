4b257a3e02bf3bb7fb0b07682be1b1b3c58538c4
edubragabonfim <ebbonfim@sga.pucminas.br>
2025-10-11 10:31:31 -0300

    Resolução 743

---
+++ 743 Network Delay Time.py
@@ -0,0 +1,38 @@
+import heapq
+
+class Solution:
+    def networkDelatyTime(self, times, n, k):
+        table = {}
+
+        for t in times:
+            if not table.get(t[0]):
+                table[t[0]] = {t[1]: t[2]}
+            else:
+                table[t[0]][t[1]] = t[2]
+
+        distances = {k: 0}
+
+        min_heap = [(0, k)]
+
+        while min_heap:
+            dist, node = heapq.heappop(min_heap)
+
+            row = table.get(node)
+
+            if row:
+                for key, v in row.items():
+                    table_dist = distances.get(key, float('inf'))
+                    if dist + v < table_dist:
+                        distances[key] = dist + v
+                        heapq.heappush(min_heap, (dist + v, key))
+        
+        _max = -1
+        if len(distances) < n:
+            return _max
+        
+        for keu, v in distances.items():
+            _max = max(_max, v)
+        
+        if _max == 0: return -1
+
+        return _max
